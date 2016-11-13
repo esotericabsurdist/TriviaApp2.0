9dcdd1c354619b43bd1eca6cc3f8b2664d2ec78c
esotericabsurdist <esotericabsurdist@gmail.com>
2016-11-13 00:47:34 -0800

    renamed some things, added socket.io

---
+++ public/index.js
@@ -0,0 +1,10 @@
+var connect = function() {
+  // set up connection.
+  var socket = io.connect('http://localhost:4200');
+  //connect, then run call back function.
+  socket.on('connect', function(data) {
+        // send string to server.
+        socket.emit('join', 'Activate Subsonic Bass');
+  });
+};
+$(document).ready(connect);
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 //configuring dependencies file
 var express = require('express');
 var parser = require('body-parser');
-var router = require('./api');
+var router = require('./trivia/router.js');
 
 var app = express();
 
@@ -13,8 +13,25 @@ require('./seed');
 app.use('/', express.static('public'));
 app.use(parser.json());
 
-app.use('/api', router);
+app.use('/', router);
 
 app.listen(3000, function() {
   console.log("The server is running on port 3000");
+});
+
+
+//socket io server
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(4200);//server.listen(process.env.PORT || 4200);
+// listen for connection
+io.sockets.on('connection', function(client) {
+
+    console.log('Client connected...');
+
+    //if client says 'join', get their data and print it to the console.
+    client.on('join', function(data) {
+        console.log(data);
+    });
+
 });
